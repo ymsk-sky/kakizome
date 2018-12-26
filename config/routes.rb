ea61614f983c2f80e01378af509848a788b4235d
ymsk.sky <ymsk.sky.95@gmail.com>
2018-12-26 12:51:58 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/top'
+  root 'static_pages#top'
 
   get 'static_pages/contact'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get top" do
-    get static_pages_top_url
+
+
+  test "should get root" do
+    get root_url
     assert_response :success
   end
 

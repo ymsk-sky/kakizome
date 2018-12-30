009e49ac3e33d895f0fc0a798fe5ef3f4dd82636
ymsk-sky <ymsk.sky.95@gmail.com>
2018-12-30 15:51:39 +0900

    implement responsive header and device-width

--- app/assets/javascripts/canvas.js
+++ app/assets/javascripts/canvas.js
@@ -1,90 +1,134 @@
 var paper_color = "#FAFAFA";
+var BLACK = "#000";
 
 (window.onload = function() {
-  var canvas = document.getElementById('mycanvas');
+  var canvas = document.getElementById("mycanvas");
+  var ctx = canvas.getContext("2d");
 
+  // サイズ調整
   var ua = navigator.userAgent;
   var iphone = ua.indexOf('iPhone') > 0;
   var android = ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0;
-  // TODO: 画面幅に合わせる
   var w = 242;
   var h = 333;
   if(iphone || android) {
-    // 高さを調整
-    canvas.height = window.innerHeight - 100;
+    // 高さを基準に決定
+    canvas.height = window.innerHeight - 160;
     canvas.width = w/h * canvas.height;
-    alert("smart phone");
   }
 
-  var ctx = canvas.getContext('2d');
-  ctx.beginPath();
   ctx.fillStyle = paper_color;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = BLACK;
 
-  var defosize = 7;
-  var defocolor = "#000";
-  var defoalpha = 1.0;
+  var mouse = {x:0, y:0, x1:0, y1:0, color:"black"};
+  var draw = false;
 
-  var mouseX = "";
-  var mouseY = "";
-
-  //各イベントに紐づけ
-  canvas.addEventListener('mousemove', onMove, false);
-  canvas.addEventListener('mousedown', onClick, false);
-  canvas.addEventListener('mouseup', drawEnd, false);
-  canvas.addEventListener('mouseout', drawEnd, false);
-
-  //マウス動いていて、かつ左クリック時に発火。
-  function onMove(e) {
-    if (e.buttons === 1 || e.witch === 1) {
-      var rect = e.target.getBoundingClientRect();
-      var X = ~~(e.clientX - rect.left);
-      var Y = ~~(e.clientY - rect.top);
-      //draw 関数にマウスの位置を渡す
-      draw(X, Y);
-    };
-  };
-
-  //マウスが左クリックされると発火。
-  function onClick(e) {
-    if (e.button === 0) {
-      var rect = e.target.getBoundingClientRect();
-      var X = ~~(e.clientX - rect.left);
-      var Y = ~~(e.clientY - rect.top);
-      //draw 関数にマウスの位置を渡す
-      draw(X, Y);
+  canvas.addEventListener("mousemove", function(e) {
+    var rect = e.target.getBoundingClientRect();
+    ctx.lineWidth = document.getElementById("lineWidth").value;
+    ctx.globalAlpha = document.getElementById("alpha").value / 100;
+    mouseX = e.clientX - rect.left;
+    mouseY = e.clientY - rect.top;
+    if(draw === true) {
+      ctx.beginPath();
+      ctx.moveTo(mouseX1, mouseY1);
+      ctx.lineTo(mouseX, mouseY);
+      ctx.lineCap = "round";
+      ctx.stroke();
+      mouseX1 = mouseX;
+      mouseY1 = mouseY;
     }
-  };
+  });
 
-  //渡されたマウス位置を元に直線を描く関数
-  function draw(X, Y) {
-    ctx.beginPath();
-    ctx.globalAlpha = defoalpha;
-    //マウス継続値によって場合分け、直線の moveTo（スタート地点）を決定
-    if (mouseX === "") {
-      //継続値が初期値の場合は、現在のマウス位置をスタート位置とする
-      ctx.moveTo(X, Y);
-    } else {
-      //継続値が初期値ではない場合は、前回のゴール位置を次のスタート位置とする
-      ctx.moveTo(mouseX, mouseY);
-    }
-    //lineTo（ゴール地点）の決定、現在のマウス位置をゴール地点とする
-    ctx.lineTo(X, Y);
-    //直線の角を「丸」、サイズと色を決める
-    ctx.lineCap = "round";
-    ctx.lineWidth = defosize * 2;
-    ctx.strokeStyle = defocolor;
-    ctx.stroke();
-    //マウス継続値に現在のマウス位置、つまりゴール位置を代入
-    mouseX = X;
-    mouseY = Y;
-  };
+  canvas.addEventListener("mousedown", function(e) {
+    draw = true;
+    mouseX1 = mouseX;
+    mouseY1 = mouseY;
+    undoImage = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  });
 
-  //左クリック終了、またはマウスが領域から外れた際、継続値を初期値に戻す
-  function drawEnd() {
-    mouseX = "";
-    mouseY = "";
+  canvas.addEventListener("mouseup", function(e) {
+    draw = false;
+  });
+
+  lineWidth.addEventListener("mousemove", function() {
+    var lineNum = document.getElementById("lineWidth").value;
+    document.getElementById("lineNum").innerHTML = lineNum;
+  });
+
+  alpha.addEventListener("mousemove",function() {
+    var alphaNum = document.getElementById("alpha").value;
+    document.getElementById("alphaNum").innerHTML = alphaNum;
+  });
+
+  $('li').click(function(){
+    ctx.strokeStyle = $(this).css('background-color');
+  });
+
+  $('#clear').click(function(e) {
+    if(!confirm('本当に消去しますか？')) return;
+    e.preventDefault();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  });
+
+  // function save() {
+  //   var can = canvas.toDataURL("image/png");
+  //   can = can.replace("image/png","image/octet-stream");
+  //   window.open(can, "save");
+  // }
+
+  // スマホ対応
+  var finger = new Array;
+  for(var i=0; i<10; i++) {
+    finger[i] = {x: 0, y: 0, x1: 0, y1: 0,
+                 color: "rgb("
+                        + Math.floor(Math.random() * 16) * 15
+                        + ", "
+                        + Math.floor(Math.random() * 16) * 15
+                        +", "
+                        + Math.floor(Math.random() * 16) * 15
+                        + ")"
+                };
   }
+
+  canvas.addEventListener("touchstart", function(e) {
+    e.preventDefault();
+    var rect = e.target.getBoundingClientRect();
+    ctx.lineWidth = document.getElementById("lineWidth").value;
+    ctx.globalAlpha = document.getElementById("alpha").value / 100;
+    undoImage = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    for(var i=0; i<finger.length; i++) {
+      finger[i].x1 = e.touches[i].clientX - rect.left;
+      finger[i].y1 = e.touches[i].clientY - rect.top;
+    }
+  });
+
+  canvas.addEventListener("touchmove", function(e) {
+    e.preventDefault();
+    var rect = e.target.getBoundingClientRect();
+    for(var i=0; i<finger.length; i++) {
+      finger[i].x = e.touches[i].clientX - rect.left;
+      finger[i].y = e.touches[i].clientY - rect.top;
+      ctx.beginPath();
+      ctx.moveTo(finger[i].x1, finger[i].y1);
+      ctx.lineTo(finger[i].x, finger[i].y);
+      ctx.lineCap = "round";
+      ctx.stroke();
+      finger[i].x1 = finger[i].x;
+      finger[i].y1 = finger[i].y;
+    }
+  });
+
+  lineWidth.addEventListener("touchmove", function() {
+    var lineNum = document.getElementById("lineWidth").value;
+    document.getElementById("lineNum").innerHTML = lineNum;
+  });
+
+  alpha.addEventListener("touchmove", function() {
+    var alphaNum = document.getElementById("alpha").value;
+    document.getElementById("alphaNum").innerHTML = alphaNum;
+  });
 })();
 
 function name_checked(ischecked) {
@@ -114,7 +158,7 @@ function clear_canvas() {
   ctx.fillStyle = paper_color;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   // ctx.clearRect(0, 0, canvas.width, canvas.height);
-
+  ctx.fillStyle = "#000";
   document.getElementById("name_check").checked = false;
 }
 

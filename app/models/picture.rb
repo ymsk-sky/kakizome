894f8e8ff80fe89d26261e2ec9313d3fa19ae873
ymsk-sky <ymsk.sky.95@gmail.com>
2018-12-31 11:42:57 +0900

    add validation uniqueness to random_token

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,4 +2,5 @@ class Picture < ApplicationRecord
   validates :theme, presence: true, length: { maximum: 20}
   validates :name, presence: true, length: { maximum: 12}
   validates :canvas_url, presence: true
+  validates :random_token, presence: true, uniqueness: true
 end
